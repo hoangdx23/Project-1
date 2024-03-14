6063a397f40235ef355344f7291e6e25d8f336f1
NTDungxPH28795 <dungndtph28795@fpt.edu.vn>
2024-03-14 18:01:43 +0700

    fix: header

--- src/pages/user/cart.tsx
+++ src/pages/user/cart.tsx
@@ -6,7 +6,6 @@ const Cart = () => {
   return (
     <div>
       <Header />
-      <Banner />
       <div className=" cart relative overflow-x-auto">
         <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
